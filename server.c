ed620592c587d1170ed48c97a5543bfa54ef3af2
Huangjun1131 <752329821@qq.com>
2018-07-19 17:11:21 +0800

    改为0xAB类型的打印

--- server.c
+++ server.c
@@ -125,7 +125,7 @@ int main()
 				int idx;
 				for (idx = 0; idx < global_count; idx++)
 				{
-					printf("0x%02X ", buf[ll->sockfd][idx]); 
+					printf("0x%02X ", (unsigned char)buf[ll->sockfd][idx]); 
 				}
 				printf("\n");
 				revents[i].events = EPOLLIN;
